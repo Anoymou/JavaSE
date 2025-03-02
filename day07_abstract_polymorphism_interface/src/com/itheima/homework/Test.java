354bf20ee570b721fd25d222dcb20b286868cc9f
zml <zml17639634346@gmail.com>
2025-03-02 11:24:01 +0800

    Changes

--- day07_abstract_polymorphism_interface/src/com/itheima/homework/Test.java
+++ day07_abstract_polymorphism_interface/src/com/itheima/homework/Test.java
@@ -2,8 +2,8 @@ package com.itheima.homework;
 
 public class Test {
     public static void main(String[] args) {
-        Manager manager =new Manager("v001","张三",13000);
-        Cook cook = new Cook("p001","小李",7000);
+        Employee manager =new Manager("v001","张三",13000);
+        Employee cook = new Cook("p001","小李",7000);
         manager.work();
         manager.eat();
         System.out.println("-----------------------");
